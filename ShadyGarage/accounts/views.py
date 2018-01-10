daf9565ac30e4720b2cc401e4fd8bd5d57b1456a
Arun Sangha <arunsangha@Aruns-MacBook-Pro.local>
2018-01-10 20:07:04 +0100

    Endringer slik at sida ser litt bedre ut..

--- ShadyGarage/accounts/views.py
+++ ShadyGarage/accounts/views.py
@@ -3,15 +3,15 @@ from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from . import models
 from . import forms
-from django.views.generic import CreateView, TemplateView
+from django.views.generic import CreateView, UpdateView, TemplateView
 # Create your views here.
 
 class SignUp(CreateView):
     form_class = forms.UserForm
     template_name="accounts/signup.html"
-    success_url = reverse_lazy("edit_profile")
+    success_url = reverse_lazy("login")
 
-class ProfileInfo(CreateView):
+class ProfileInfo(CreateView, LoginRequiredMixin):
     form_class = forms.ProfileForm
     template_name = "accounts/profile_info.html"
     success_url = reverse_lazy("login")
