e6617bca6f7e3129eb3e0f46e8db7937b3df66d4
Arun Sangha <arun.sangha@gmail.com>
2018-01-12 15:19:22 +0100

    Endelig funker det å registrere seg forså og legge til profilbilde, og team

--- ShadyGarage/accounts/views.py
+++ ShadyGarage/accounts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import authenticate, login, logout
 from . import models
 from . import forms
 from django.views.generic import CreateView, UpdateView, TemplateView
@@ -10,6 +11,21 @@ class SignUp(CreateView):
     form_class = forms.UserForm
     template_name="accounts/signup.html"
     success_url = reverse_lazy("login")
+
+def signup(request):
+    registered = False
+    if request.method == 'POST':
+        user_form = forms.UserForm(data = request.POST)
+
+        if user_form.is_valid():
+            new_user = user_form.save()
+            new_user = authenticate(username = user_form.cleaned_data['username'], password = user_form.cleaned_data['password1'],)
+            login(request, new_user)
+            return redirect("accounts:signup_del2")
+
+    else:
+        user_form = forms.UserForm()
+    return render(request, "accounts/signup.html", {'user_form': user_form})
 
 class ProfileInfo(CreateView, LoginRequiredMixin):
     form_class = forms.ProfileForm
