4ad27e53e14f931b4f6a42ef073529b55cff8a71
Arun Sangha <arun.sangha@gmail.com>
2018-02-09 15:22:48 +0100

    List view pagination

--- ShadyGarage/posts/api/serializers.py
+++ ShadyGarage/posts/api/serializers.py
@@ -1,15 +1,28 @@
 from rest_framework import serializers
+from django.utils.timesince import timesince
 from posts.models import Post
-
+from accounts.api import serializers as accounts_serialisers
 class PostModelSerializer(serializers.ModelSerializer):
-    #user = ProfileDisplaySerializer()
+    user_fk = accounts_serialisers.UserDisplaySerializer()
+    date_display = serializers.SerializerMethodField()
+    timesince = serializers.SerializerMethodField()
     class Meta:
         model = Post
         fields = (
-            'post_title',
             'user_fk',
+            'post_title',
             'post_image',
             'post_description',
             'post_likes',
             'post_created',
+            'date_display',
+            'timesince',
         )
+
+    def get_date_display(self, obj):
+        return obj.post_created.strftime("%b %d %I:%M %p ")
+
+    def get_timesince(self, obj):
+        return timesince(obj.post_created) + " siden"
+
+    
--- ShadyGarage/posts/api/views.py
+++ ShadyGarage/posts/api/views.py
@@ -1,9 +1,21 @@
 from rest_framework import generics
 from posts.models import Post
 from .serializers import PostModelSerializer
+from django.db.models import Q
+from .pagination import StandardResultsPagination
 
 class PostListAPIView(generics.ListAPIView):
     serializer_class = PostModelSerializer
+    pagination_class = StandardResultsPagination
 
     def get_queryset(self):
-        return Post.objects.all()
+        qs = Post.objects.all()
+        query = self.request.GET.get("q", None)
+        if query is not None:
+            qs = qs.filter(
+                Q(user_fk__username__icontains=query) |
+                Q(post_title__icontains=query) |
+                Q(post_description__icontains=query) |
+                Q(post_created__icontains=query)
+            )
+        return qs
