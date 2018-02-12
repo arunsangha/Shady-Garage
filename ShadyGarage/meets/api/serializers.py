3ac086abdef46f52a8a3e3cc59540cfe53b15821
Arun Sangha <arun.sangha@gmail.com>
2018-02-12 22:52:13 +0100

    Error fra git..?

--- ShadyGarage/meets/api/serializers.py
+++ ShadyGarage/meets/api/serializers.py
@@ -2,8 +2,12 @@ from rest_framework import serializers
 from accounts.api.serializers import UserDisplaySerializer
 from meets.models import Meet
 
+
 class MeetsModelSerializer(serializers.ModelSerializer):
     user_fk = UserDisplaySerializer()
+    day = serializers.SerializerMethodField()
+    date = serializers.SerializerMethodField()
+    time = serializers.SerializerMethodField()
     class Meta:
         model = Meet
         fields = (
@@ -14,4 +18,14 @@ class MeetsModelSerializer(serializers.ModelSerializer):
             'description',
             'meet_image',
             'users_joining',
+            'day',
         )
+
+    def get_day(self, obj):
+        return obj.date.strftime("%A")
+
+    def get_date(self, obj):
+        return obj.date.strftime("%Y.%m.%d")
+
+    def get_time(self, obj):
+        return obj.time.strftime("%H:%M")
