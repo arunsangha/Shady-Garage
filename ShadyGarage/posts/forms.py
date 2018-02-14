a57d042e01282bb55e2d026c71b7700d55d2562d
Arun Sangha <arun.sangha@gmail.com>
2018-02-14 20:16:02 +0100

    Fikset en bug og muligheten for å poste bilder

--- ShadyGarage/posts/forms.py
+++ ShadyGarage/posts/forms.py
@@ -12,6 +12,15 @@ class PostForm(forms.ModelForm):
         self.fields['post_image'].label = "Bilde"
         self.fields['post_description'].label = "Beskrivelse"
 
+    def clean_image(self):
+        image = self.cleaned_data.get('profile_pic', False)
+        if image:
+            if image._size > 4*700*700:
+                raise ValidationError("Image file too large ( > 4mb )")
+            return image
+        else:
+            raise ValidationError("Couldn't read uploaded image")
+
 class PostCommentForm(forms.ModelForm):
     class Meta():
         model = models.PostComment
--- ShadyGarage/posts/urls.py
+++ ShadyGarage/posts/urls.py
@@ -5,9 +5,9 @@ app_name = 'posts'
 
 urlpatterns = [
     url(r'^post_feed/$', views.PostList.as_view(), name="posts_feed"),
-    url(r'^(?P<slug>[-\w]+)/$', views.PostDetail.as_view(), name="post_detail"),
+    url(r'^shady/(?P<slug>[-\w]+)/$', views.PostDetail.as_view(), name="post_detail"),
     url(r'^new_post/$', views.PostForm.as_view(), name="post_create"),
-    url(r'^(?P<slug>[-\w]+)/delete/$', views.PostDeleteView.as_view(), name="post_delete"),
-    url(r'^(?P<slug>[-\w]+)/comment/$', views.PostCommentCreateView.as_view(), name="post_comment"),
-    url(r'^(?P<slug>[-\w]+)/comment/(?P<pk>\d+)/delete/$', views.PostCommentDeleteView.as_view(), name="post_comment_delete"),
+    url(r'^shady/(?P<slug>[-\w]+)/delete/$', views.PostDeleteView.as_view(), name="post_delete"),
+    url(r'^shady/(?P<slug>[-\w]+)/comment/$', views.PostCommentCreateView.as_view(), name="post_comment"),
+    url(r'^shady/(?P<slug>[-\w]+)/comment/(?P<pk>\d+)/delete/$', views.PostCommentDeleteView.as_view(), name="post_comment_delete"),
 ]
