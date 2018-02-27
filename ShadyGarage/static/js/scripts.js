5752e414d11b74c8fd7bd108e13ad63f53e08afc
Christian Thorby <christianthorby@hotmail.com>
2018-02-27 14:23:06 +0100

    lukking av mobil meny ved trykk utenfor meny

--- ShadyGarage/static/js/scripts.js
+++ ShadyGarage/static/js/scripts.js
@@ -1,17 +1,43 @@
 function initMap() {
-     var oslo = {lat: 59.913, lng: 10.752};
-     var map = new google.maps.Map(document.getElementById('map'), {
-       zoom: 6,
-       center: oslo
-     });
-     var marker = new google.maps.Marker({
-       position: oslo,
-       map: map
-     });
-   }
-
-   $(document).ready(function() {
-   	$(".burger-nav").on('click', function() {
-   		$("div ul").toggleClass("nav-list-open");
-   	});
+   var oslo = {lat: 59.913, lng: 10.752};
+   var map = new google.maps.Map(document.getElementById('map'), {
+     zoom: 6,
+     center: oslo
    });
+   var marker = new google.maps.Marker({
+     position: oslo,
+     map: map
+   });
+}
+
+$(document).ready(function() {
+$(".burger-nav").on('click', function() {
+   $("div ul").toggleClass("nav-list-open");
+});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//TODO sjekk om dette funker
+// const burgerNav = document.querySelector(".burger-nav");
+//
+// burgerNav.addEventListener("blur", hideBurgerWhenClickElsewhere);
+//
+// function hideBurgerWhenClickElsewhere(e){
+//      burgerNav.classList.remove("nav-list-open");
+// }
