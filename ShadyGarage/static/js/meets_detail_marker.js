1f0fbd68c4219b24073944ec48fe57cf1d797244
Arun Sangha <arun.sangha@gmail.com>
2018-03-16 00:15:47 +0100

    Stylet markør i meets detail

--- ShadyGarage/static/js/meets_detail_marker.js
+++ ShadyGarage/static/js/meets_detail_marker.js
@@ -42,6 +42,157 @@ function initMap() {
      var map = new google.maps.Map(document.getElementById('map'), {
        zoom: 15,
        center: oslo,
+       styles: [
+{
+    "featureType": "all",
+    "elementType": "labels.text.fill",
+    "stylers": [
+        {
+            "color": "#ffffff"
+        }
+    ]
+},
+{
+    "featureType": "all",
+    "elementType": "labels.text.stroke",
+    "stylers": [
+        {
+            "color": "#000000"
+        },
+        {
+            "lightness": 13
+        }
+    ]
+},
+{
+    "featureType": "administrative",
+    "elementType": "geometry.fill",
+    "stylers": [
+        {
+            "color": "#000000"
+        }
+    ]
+},
+{
+    "featureType": "administrative",
+    "elementType": "geometry.stroke",
+    "stylers": [
+        {
+            "color": "#144b53"
+        },
+        {
+            "lightness": 14
+        },
+        {
+            "weight": 1.4
+        }
+    ]
+},
+{
+    "featureType": "administrative.locality",
+    "elementType": "all",
+    "stylers": [
+        {
+            "visibility": "on"
+        }
+    ]
+},
+{
+    "featureType": "administrative.locality",
+    "elementType": "labels.icon",
+    "stylers": [
+        {
+            "visibility": "on"
+        }
+    ]
+},
+{
+    "featureType": "landscape",
+    "elementType": "all",
+    "stylers": [
+        {
+            "color": "#08304b"
+        }
+    ]
+},
+{
+    "featureType": "poi",
+    "elementType": "geometry",
+    "stylers": [
+        {
+            "color": "#0c4152"
+        },
+        {
+            "lightness": 5
+        }
+    ]
+},
+{
+          featureType: 'road.highway',
+          elementType: 'geometry.stroke',
+          stylers: [{color: '#1f2835'}]
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#f3d19c'}]
+        },
+{
+    "featureType": "road.arterial",
+    "elementType": "geometry.fill",
+    "stylers": [
+        {
+            "color": "#000000"
+        }
+    ]
+},
+{
+    "featureType": "road.arterial",
+    "elementType": "geometry.stroke",
+    "stylers": [
+        {
+            "color": "#0b3d51"
+        },
+        {
+            "lightness": 16
+        }
+    ]
+},
+{
+    "featureType": "road.local",
+    "elementType": "geometry",
+    "stylers": [
+        {
+            "color": "#000000"
+        }
+    ]
+},
+{
+    "featureType": "transit",
+    "elementType": "all",
+    "stylers": [
+        {
+            "color": "#146474"
+        }
+    ]
+},
+{
+    "featureType": "water",
+    "elementType": "all",
+    "stylers": [
+        {
+            "color": "#021019"
+        }
+    ]
+},
+{
+    featureType: "administrative.country",
+    elementType: "geometry.stroke",
+    stylers: [
+        { color: "#FF0000" }
+    ]
+  },
+]
      });
 
    }
