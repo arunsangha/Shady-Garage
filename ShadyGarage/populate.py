d33c403afdaabf971afaed762549e7e8ebb01b5c
Arun Sangha <arun.sangha@gmail.com>
2018-03-16 00:42:13 +0100

    Lagt til litt mer i faker

--- ShadyGarage/populate.py
+++ ShadyGarage/populate.py
@@ -14,10 +14,15 @@ from posts.models import Post, PostComment
 
 users = User.objects.all()
 
-meet_name = ['Burning i Halden!', 'Street meet Ikea!', 'Volvo entusiaster, Frogner!', 'Stance meet!', 'Slottsfjell Offroad Treff']
-comments = ['Kult', 'Hvor kjøpte du bilen?', 'Hvor kjøpte du felgene?', 'Nice', ':D', ';)', 'Fin bil', 'Jævlig kul bil']
-location = ['Frognerveien 13, 0263 Oslo', 'Storgaten 12, 3126 Tønsberg', 'Furusetveien12, 1058 Oslo', 'Altagårdskogen 32, Alta', 'Klæbuveien 127, 7031 Trondheim']
-post_title = ['Ute med bae!', 'Godt med ny vasket bil!', 'Vinter er herlig!', 'Nye sko til bilen :)', 'Æ elsk hu!']
+meet_name = ['Burning i Halden!', 'Street meet Ikea!', 'Volvo entusiaster, Frogner!', 'Stance meet!', 'Slottsfjell Offroad Treff'
+            'Norgest kuleste treff', 'Bare en rolig kveld', 'Kun stjerner', 'VAG MEET', 'Cars & Coffe']
+comments = ['Kult', 'Hvor kjøpte du bilen?', 'Hvor kjøpte du felgene?', 'Nice', ':D', ';)', 'Fin bil', 'Jævlig kul bil'
+            'Å fy fader asss!', 'For en kveld :D', 'Ræægen for livet!', 'Fet kar fet bil', 'Whalla spa bil mann']
+location = ['Frognerveien 13, 0263 Oslo', 'Storgaten 12, 3126 Tønsberg', 'Furusetveien12, 1058 Oslo', 'Altagårdskogen 32, Alta',
+            'Klæbuveien 127, 7031 Trondheim', 'Fjøsangerveien 4, 5008 Bergen', ' Henrik Wergelands gate 27, 4612 Kristiansand',
+            'Sjusjøen-pihl 4328, 2612 Sjusjøen', 'Kirkegaten 31B, Gamle Fredrikstad', 'Kviteseid kommune, Kviteseidgata, 3850 Kviteseid']
+post_title = ['Ute med bae!', 'Godt med ny vasket bil!', 'Vinter er herlig!', 'Nye sko til bilen :)', 'Æ elsk hu!'
+              'Godt med sommerdekk', 'Nom nom', 'Nye glisset', '"Skriv tittel her:" Æ vet a faen', 'BMW BRUUUUUR']
 
 def add_meets(N=5):
     meet_, created_or_not = Meet.objects.get_or_create(user_fk= user_[N], meet_name=meet_name[N], date=fake.date_this_month(before_today=True, after_today=False),
@@ -55,7 +60,7 @@ def delete_objects():
     Post.objects.all().delete()
     Meet.objects.all().delete()
 
-def populate(N=5):
+def populate(N=10):
     for entry in range(N):
         meet_ = add_meets(entry)
         post__ = add_posts(entry)
