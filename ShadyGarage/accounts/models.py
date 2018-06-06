971193828e99b2c7d630f7fd3e826f341f9ec623
Arun Sangha <arun.sangha@gmail.com>
2018-06-06 21:08:14 +0200

    Fikset bug ved registrering når man ikke valgte teams etc..

--- ShadyGarage/accounts/models.py
+++ ShadyGarage/accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib import auth
 from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 class User(auth.models.User, auth.models.PermissionsMixin):
 
@@ -64,8 +65,7 @@ class Profile(models.Model):
 
         super().save()
 
-def create_profile(sender, **kwargs):
-    if kwargs['created']:
-        profile = Profile.objects.create(users = kwargs['instance'])
-
-post_save.connect(create_profile, sender = User)
+@receiver(post_save, sender=auth.models.User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance, teams="SHADYGARAGE")
