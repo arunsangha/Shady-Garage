e835083d1f411fe5f874009bce2e057591c364ea
Arun Sangha <arun.sangha@gmail.com>
2018-07-21 02:09:27 +0200

    Changed password on email host

--- ShadyGarage/ShadyGarage/settings/production.py
+++ ShadyGarage/ShadyGarage/settings/production.py
@@ -143,6 +143,6 @@ LOGOUT_REDIRECT_URL = "home"
 
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = 'shadygarage.no@gmail.com'
-EMAIL_HOST_PASSWORD = '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'
+EMAIL_HOST_PASSWORD = 'skriveboktavle97'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
