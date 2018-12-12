e2bfbd53bce403fbd170a74d449745debc489db9
Arun Sangha <arun.sangha@gmail.com>
2018-12-12 16:20:49 +0100

    Created model..needs normalizing

--- ShadyGarage/blogs/models.py
+++ ShadyGarage/blogs/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title                = models.CharField(max_length=200)
+    price                = models.PositiveIntegerField()
+    engine               = models.CharField(max_length=50)
+    zero_to_100          = models.DecimalField(max_digits=2, decimal_places=2)
+    consumption          = models.DecimalField(max_digits=2, decimal_places=2)
+    top_image            = models.ImageField(upload_to="blogs/top_image")
+    intro_image          = models.ImageField(upload_to="blogs/intro_image")
+    intro_title          = models.CharField(max_length=70)
+    intro_paragraph      = models.CharField(max_length=600)
+    gif                  = models.ImageField(upload_to="blogs/gif")
+    paragraph_one_title  = models.CharField(max_length=70)
+    paragraph_one        = models.CharField(max_length=2000)
+    youtube_link         = models.CharField(max_length=1000)
+    first_parallax       = models.ImageField(upload_to="blogs/parallax_1")
+    paragraph_two_title  = models.CharField(max_length=70)
+    paragraph_two        = models.CharField(max_length=2000)
+    paragraph_two_image  = models.ImageField(upload_to="blogs/paragraph_3")
+    second_parallax       = models.ImageField(upload_to="blogs/parallax_2")
+    paragraph_three_title  = models.CharField(max_length=70)
+    paragraph_three        = models.CharField(max_length=2000)
+    score                = models.PositiveIntegerField()
