b8a890f997d7b99457ffd8c446a23fcb7b935536
Arun Sangha <arun.sangha@gmail.com>
2019-01-08 21:01:32 +0100

    Ip address error

--- ShadyGarage/analytics/utils.py
+++ ShadyGarage/analytics/utils.py
@@ -1,6 +1,6 @@
 
 def get_client_ip(request):
-    x_forwared_for = request.META.get('HTTP_X_FORWARED_FOR')
+    x_forwared_for = request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwared_for:
         ip = x_forwared_for.split(",")[0]
     else:
